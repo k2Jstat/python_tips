d09e8cc96bffaabbb400c454c1f9acb70f63bf0c
unknown <debreulang@gmail.com>
2018-12-04 21:46:41 +0900

    add scriot

---
+++ script/check_object.py
@@ -0,0 +1,15 @@
+def check_valiables(variable_size_threshold = 0,list_dir = dir()):
+    """
+    input   : none
+    output  : variables
+              
+    example : dist_by_cat(variable_size_threshold = 10000)  
+     """
+    import sys
+    
+    print("{}{: >30}{}{: >16}{}".format('|','Variable Name','|','Memory','|'))
+    print(" ----------------------------------------------- ")
+    
+    for var_name in list_dir:
+        if not var_name.startswith("_") and sys.getsizeof(eval(var_name)) >= variable_size_threshold:
+            print("{}{: >30}{}{: >16,}{}".format('|',var_name,'|',sys.getsizeof(eval(var_name)),'|'))
